f204b776ba47bfac1bfc3d6f6324a4d421a1e096
“gabriel6181997” <gabriel6181997@gmail.com>
2020-12-31 22:55:26 +0800

    ちゃ

--- js/script.js
+++ js/script.js
@@ -4,25 +4,24 @@ const input = document.getElementById('myinput');
 const ul = document.getElementById('myUL');
 const li = ul.getElementsByTagName('li');
 
-//click event for new list item
+//Click event for new list item
 button.addEventListener('click',(e)=>{
   e.preventDefault();
   if(input.value !==''){
-
     //Input data
     const newli = document.createElement('li');
     newli.textContent = input.value;
-
+    
     //Add(remove) class .checked to(from) new list item
     newli.addEventListener('click',()=>{
       newli.classList.toggle('checked');
     });
-
+    
     //Back to data input
     ul.appendChild(newli);
     ul.insertAdjacentText = ('afterend', newli);
     input.value = "";
-
+    
     //Create cross button for new list item
     const span = document.createElement('span');
     const cross = document.createTextNode("\u00D7");
@@ -34,15 +33,6 @@ button.addEventListener('click',(e)=>{
   }
 }); 
 
-//Add(remove) class .checked to (from) old list item
-for(let i=0; i<li.length; i++){
-  li[i].addEventListener('click',(e)=>{
-    e.preventDefault();
-    //add .parentnode to ensure that .checked class is not added to span when the task is clicked
-    const target = e.target.parentNode;
-    target.classList.toggle('checked');
-  });
-};
 
 //Create cross button for old list item
 for(let u=0; u<li.length;u++){
@@ -52,6 +42,21 @@ for(let u=0; u<li.length;u++){
   span.appendChild(cross);
   li[u].appendChild(span);
 };
+
+//Add(remove) class .checked to (from) old list item
+for(let i=0; i<li.length; i++){
+  li[i].addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(e.target.nodeName === 'LI') {
+      const target = e.target;
+      target.classList.toggle('checked');      
+    } return
+  });
+};
+
+
+//Delete task
+
 
 
 
